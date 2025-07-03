aec7c3a481f2ff53c45b2c76b967f8b3b7a08fc9
khushi703 <zalavadiyakhushi80@gmail.com>
2025-07-04 00:45:07 +0530

    feat: Return number itself for single input

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.Objects;
+
 public class StringCalculator {
     public static int add(String input) {
-        return 0;
+        if(Objects.equals(input, "")) {
+            return 0;
+        }
+        return Integer.parseInt(input);
     }
 }
