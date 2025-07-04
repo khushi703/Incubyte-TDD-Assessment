8f9c4491af7803a8fa3b2094ab6a003a469772f0
khushi703 <zalavadiyakhushi80@gmail.com>
2025-07-04 17:22:02 +0530

    feat: support custom single-character delimiter in Add method

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,14 +1,21 @@
 package org.example;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     public static int add(String input) {
         if (input == null || input.isEmpty()) { // if input is ""
             return 0;
         }
-        // use a constant for delimiters for clarity and in future addition of new delimiters
-        final String DELIMITER = "[,\n]";
+
+        String DELIMITER = "[,\n]";
+        // Check for custom delimiter syntax
+        if (input.startsWith("//")) {
+            int delimiterEnd = input.indexOf('\n');
+            DELIMITER = Pattern.quote(input.substring(2, delimiterEnd));
+            input = input.substring(delimiterEnd + 1);
+        }
         String[] numbersArray = input.split(DELIMITER);
         int sumOfArray = 0;
         for (String number : numbersArray) {
