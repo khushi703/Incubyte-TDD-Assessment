67e2855b68ef657a518794902a55c76f98c5e068
khushi703 <zalavadiyakhushi80@gmail.com>
2025-07-04 18:08:20 +0530

    test: verify Add ignores numbers greater than 1000

--- src/main/test/java/org/example/StringCalculatorTest.java
+++ src/main/test/java/org/example/StringCalculatorTest.java
@@ -33,4 +33,10 @@ public class StringCalculatorTest {
             assertEquals("Negatives not allowed: [-1,-8]", e.getMessage());
         }
     }
+    @Test
+    public void testCanIgnoreNumberGraterThen1000(){
+        assertEquals(2, StringCalculator.add("2,1001"));
+        assertEquals(7, StringCalculator.add("7,1001"));
+        assertEquals(1002, StringCalculator.add("2,1000"));
+    }
 }
