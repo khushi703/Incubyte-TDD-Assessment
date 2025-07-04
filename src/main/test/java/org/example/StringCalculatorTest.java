5a0ec11df4c1495920a9de1cc63b0936746be1e1
khushi703 <zalavadiyakhushi80@gmail.com>
2025-07-04 17:12:25 +0530

    test: verify custom single-character delimiter

--- src/main/test/java/org/example/StringCalculatorTest.java
+++ src/main/test/java/org/example/StringCalculatorTest.java
@@ -18,5 +18,10 @@ public class StringCalculatorTest {
     @Test
     public void testCanHandleNewlineAsDelimiter() {
         assertEquals(6, StringCalculator.add("1\n2,3"));
+        assertEquals(1,StringCalculator.add("1,\n"));
+    }
+    @Test
+    public void testCanHandleCustomDelimiter() {
+        assertEquals(7, StringCalculator.add("//;\n3;4"));
     }
 }
